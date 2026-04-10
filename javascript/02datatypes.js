e433a5e23fdfeed6db4c3224ec10677fca83c632
Dhruv Patil <dhruv.patilb2305@gmail.com>
2026-04-10 16:04:34 +0000

    here we understood the datatypes

---
+++ javascript/02datatypes.js
@@ -0,0 +1,9 @@
+console.log(typeof 'dhruv');
+console.log(typeof 3);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+// null indicates empty it does not have value but it also cannot be zero 
+// null is indicated as object this i think its a javascript language error 
+
