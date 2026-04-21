e6c5b768e8ae8f12116500bb0a6b397cf34b3d2c
Dhruv Patil <dhruv.patilb2305@gmail.com>
2026-04-21 10:17:11 +0000

    learned the basic comparison and its rules

---
+++ javascript/comparison.js
@@ -0,0 +1,27 @@
+//console.log(2>0);
+//console.log(2<0);
+//console.log(2>=0);
+//console.log(2<=0);
+//console.log(2==2);
+//basic comparison operators we use in javascript
+
+//console.log("2">1);
+//console.log("2"<3);
+//console.log("2"==2);
+//avoid these type of coding in javascript its not clean. giving log in boolean as true but basically its comparing the values not datatypes.
+
+// === this is used for strict check equality for comparison and dataype both.
+
+console.log("2"===2);// this gives log false for not having same datatype one is string and another is number.
+console.log(2===2);// here it gives log true for comparing and having same datatype as number.
+
+//same we can do comparison for null and undefined datatype as same.
+
+
+
+
+
+
+
+
+
