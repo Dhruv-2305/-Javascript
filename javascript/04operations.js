e9ea9a90ad0e44fefb1673c439f01f03e20787ce
Dhruv Patil <dhruv.patilb2305@gmail.com>
2026-04-21 10:16:49 +0000

    learned the basic operations and its rules

---
+++ javascript/04operations.js
@@ -0,0 +1,43 @@
+let num1 = 4
+let num2 = 5
+//console.log(num1 + num2)
+
+//console.log(x+y)
+//console.log(x-y)
+//console.log(x*y)
+//console.log(x/y)
+//console.log(x%y)
+//basic arithmetic operations in js 
+
+let firstName = 'Dhruv'
+let secondName = ' Patil'
+let fullName = firstName+ secondName
+//console.log(fullName)
+
+//console.log("1"+2);
+//console.log(1+"2")
+//console.log("1"+1+2);//here we take at prefix so it thinks like its string and prints 112
+//console.log(1+1+"2");// here we take at postfix so it thinks like its number and adds them last number 2 prints as string as it is double quotes.
+// prints 22
+
+let gameCounter= 100
+gameCounter++
+//console.log(gameCounter)// here it prints 101 as it increments the number 100. we used postfix 
+
+let gameCounter1= 100
+++gameCounter1
+//console.log(gameCounter1)//here as same as above it prints 101 .we used prefix 
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
