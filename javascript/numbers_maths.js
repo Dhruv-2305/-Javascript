570e767e4ed708fdfac41f61ad963f55410ac1dc
Dhruv Patil <dhruv.patilb2305@gmail.com>
2026-04-21 13:53:28 +0000

    learned about maths section where we used math.random and math.floor

---
+++ javascript/numbers_maths.js
@@ -0,0 +1,9 @@
+// in this file we discovered the numbers to be converted tostring, toprecision, toexponential, tofixed in browsers console 
+// please do check at your own 
+
+//example
+const balance = new Number(200)
+console.log(balance)
+
+// it will create the Number 200 strictly a number 
+// we can assign them tostring, to precision, toexponential,  tofixed.
