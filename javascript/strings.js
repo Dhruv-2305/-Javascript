73b52791ea931314f495d9c8fa8a039889c9f501
Dhruv Patil <dhruv.patilb2305@gmail.com>
2026-04-21 12:20:32 +0000

    strings.js learned the string and how to operate it

---
+++ javascript/strings.js
@@ -0,0 +1,12 @@
+const myName= "Dhruv"
+const likeCount= 500
+
+console.log(`Hi my name is ${myName} and my profile has like count ${likeCount}`)// this is called string interpolation the best practice in javascript to concatenate the strings to each other
+
+const lastName = "viking"
+const copyName = 10000000
+console.log(`hi my last name is ${lastName} has like count ${copyName}`)
+
+    const gameName = new String ('dhruv')
+    console.log(gameName);
+    // here we checked the how Strings operate and what methods we learned and properties in console
