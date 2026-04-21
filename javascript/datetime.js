fb3132d785ce333f58f00dbec49102f880439cba
Dhruv Patil <dhruv.patilb2305@gmail.com>
2026-04-21 15:05:34 +0000

    worked on date and time with its objects

---
+++ javascript/datetime.js
@@ -0,0 +1,15 @@
+let mydate = new Date()
+//console.log(mydate.toDateString())
+//console.log(mydate.toISOString())
+//console.log(mydate.toJSON())
+//console.log(mydate.toLocaleDateString())
+//console.log(mydate.toLocaleString())
+//console.log(mydate.toLocaleTimeString())
+//console.log(mydate.toString())
+//console.log(mydate.toUTCString())
+// so these are some date object with its methods with (mydate.to......())
+
+let mynewDate = new Date(2026,4,21,20,23)
+console.log(mynewDate.toLocaleString())
+// here we created live date example with time log mynewDate with method tolocalestring().
+
--- javascript/numbers_maths.js
+++ javascript/numbers_maths.js
@@ -3,7 +3,20 @@
 
 //example
 const balance = new Number(200)
-console.log(balance)
+//console.log(balance)
 
 // it will create the Number 200 strictly a number 
 // we can assign them tostring, to precision, toexponential,  tofixed.
+
+//+++++++ maths ++++++++
+
+console.log(Math.floor(Math.random()));
+
+const min =10 
+const max = 11
+
+console.log((Math.random()*(max - min +1))+ min);
+
+//math.random() gives the value between 0 and 1 .
+
+//math.floor is function used to have precise round off value.
